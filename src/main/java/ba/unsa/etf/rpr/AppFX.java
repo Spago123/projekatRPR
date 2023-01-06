32192bdf267b5b65ff60658a0f688c37783aeda1
Spago123 <hspago1@etf.unsa.ba>
2023-01-06 21:46:33 +0100

    AppFX done

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.controllers.HomeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,12 +7,26 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class AppFX extends Application {
 
-    public static void main(String[] args) {
+    static Map<String, String> pageNames = new TreeMap<>();
+
+    public static void main(String[] args) throws IOException {
+        Properties p = new Properties();
+        p.load(ClassLoader.getSystemResource("application.properties.pageTitles").openStream());
+        pageNames.put("loginPage", p.getProperty("loginPage"));
+        pageNames.put("patientHome", p.getProperty("patientHome"));
+        pageNames.put("doctorHome", p.getProperty("doctorHome"));
+        pageNames.put("editPatient", p.getProperty("editPatient"));
+        pageNames.put("addDiagnosis", p.getProperty("addDiagnosis"));
+        pageNames.put("viewHistory", p.getProperty("viewHistory"));
+
         launch(args);
     }
 
@@ -28,5 +41,9 @@ public class AppFX extends Application {
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public static String getPageTitle(String key){
+        return pageNames.get(key);
     }
 }
