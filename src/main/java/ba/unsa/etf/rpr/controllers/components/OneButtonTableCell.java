afdd973f3f09853060dbf1c54c82694f85b61a67
Spago123 <hspago1@etf.unsa.ba>
2023-01-06 23:21:52 +0100

    One button cell table cell radi

--- src/main/java/ba/unsa/etf/rpr/controllers/components/OneButtonTableCell.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/components/OneButtonTableCell.java
@@ -2,8 +2,10 @@ package ba.unsa.etf.rpr.controllers.components;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
+import javafx.scene.layout.HBox;
 
 public class OneButtonTableCell<T> extends TableCell<T, T> {
 
@@ -13,6 +15,15 @@ public class OneButtonTableCell<T> extends TableCell<T, T> {
         view.setOnAction(buttonOne);
     }
 
-
-
+    @Override
+    protected void updateItem(T o, boolean b) {
+        super.updateItem(o, b);
+        if (o != null) {
+            HBox box = new HBox();
+            box.setAlignment(Pos.CENTER);
+            view.setUserData(o);
+            box.getChildren().add(view);
+            setGraphic(box);
+        }
+    }
 }
